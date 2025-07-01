35acfa4a2a7deb6b6a5274be4805a43f9f3399e2
Muhammed Murat Yanaşoğlu <akademiyanasoglu@gmail.com>
2025-07-01 23:12:35 +0300

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+from muratlib import add, subtract, multiply, divide, power
+
+def main():
+    print("57 + 12 = ", add(5, 7))
+    print("5 - 211 = ", subtract(10, 2))
+    print("14 * 4 = ", multiply(3, 4))
+    print("12 / 2 = " , divide(16, 2))
+    print("2 power 8 = ", power(2, 8))
+
+if __name__ == "__main__":
+    main()
